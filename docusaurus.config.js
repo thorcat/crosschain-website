046cf3c8b5b185efcc0e9b6cc865365c9a02a060
thorcat <thorcatstevens@protonmail.com>
2021-12-13 11:21:48 -0800

    edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,13 +25,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          editUrl: 'https://github.com/thorcat/crosschain-website',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+            'https://github.com/thorcat/crosschain-website/edit/main/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
